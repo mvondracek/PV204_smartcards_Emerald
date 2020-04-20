93f496043eeba46df87146c33968fd214b890a6e
Martin Vondráček <vondracek.mar@gmail.com>
2020-04-20 23:00:19 +0200

    added JCMathLib with basic usage example

--- applet/src/main/java/applet/EmeraldApplet.java
+++ applet/src/main/java/applet/EmeraldApplet.java
@@ -13,6 +13,10 @@ import javacard.framework.ISO7816;
 import javacard.framework.ISOException;
 import javacard.framework.MultiSelectable;
 import javacard.framework.Util;
+import applet.jcmathlib.ECConfig;
+import applet.jcmathlib.ECPoint;
+import applet.jcmathlib.ECCurve;
+import applet.jcmathlib.SecP256r1;
 
 public class EmeraldApplet extends Applet implements MultiSelectable {
     public static final byte PIN_LENGTH = 4;
@@ -23,6 +27,14 @@ public class EmeraldApplet extends Applet implements MultiSelectable {
      */
     private final byte[] pin; // TODO store pin securely
 
+    //region TODO example use of JCMathlib
+    ECConfig        ecc = null;
+    ECCurve         curve = null;
+    ECPoint         point1 = null;
+    ECPoint         point2 = null;
+    final static byte[] ECPOINT_TEST_VALUE = {(byte)0x04, (byte) 0x3B, (byte) 0xC1, (byte) 0x5B, (byte) 0xE5, (byte) 0xF7, (byte) 0x52, (byte) 0xB3, (byte) 0x27, (byte) 0x0D, (byte) 0xB0, (byte) 0xAE, (byte) 0xF2, (byte) 0xBC, (byte) 0xF0, (byte) 0xEC, (byte) 0xBD, (byte) 0xB5, (byte) 0x78, (byte) 0x8F, (byte) 0x88, (byte) 0xE6, (byte) 0x14, (byte) 0x32, (byte) 0x30, (byte) 0x68, (byte) 0xC4, (byte) 0xC4, (byte) 0x88, (byte) 0x6B, (byte) 0x43, (byte) 0x91, (byte) 0x4C, (byte) 0x22, (byte) 0xE1, (byte) 0x67, (byte) 0x68, (byte) 0x3B, (byte) 0x32, (byte) 0x95, (byte) 0x98, (byte) 0x31, (byte) 0x19, (byte) 0x6D, (byte) 0x41, (byte) 0x88, (byte) 0x0C, (byte) 0x9F, (byte) 0x8C, (byte) 0x59, (byte) 0x67, (byte) 0x60, (byte) 0x86, (byte) 0x1A, (byte) 0x86, (byte) 0xF8, (byte) 0x0D, (byte) 0x01, (byte) 0x46, (byte) 0x0C, (byte) 0xB5, (byte) 0x8D, (byte) 0x86, (byte) 0x6C, (byte) 0x09};
+    final static byte[] SCALAR_TEST_VALUE = {(byte) 0xE8, (byte) 0x05};
+    //endregion
 
     /**
      * Create instance of the applet.
@@ -59,6 +71,15 @@ public class EmeraldApplet extends Applet implements MultiSelectable {
         pin = new byte[PIN_LENGTH];
         Util.arrayCopy(bArray, appletDataOffset, pin, (short) 0, PIN_LENGTH);
 
+        //region TODO example use of JCMathlib
+        // Pre-allocate all helper structures
+        ecc = new ECConfig((short) 256);
+        // Pre-allocate standard SecP256r1 curve and two EC points on this curve
+        curve = new ECCurve(false, SecP256r1.p, SecP256r1.a, SecP256r1.b, SecP256r1.G, SecP256r1.r);
+        point1 = new ECPoint(curve, ecc.ech);
+        point2 = new ECPoint(curve, ecc.ech);
+        //endregion
+
         // initialization successful
         register();
     }
@@ -91,6 +112,21 @@ public class EmeraldApplet extends Applet implements MultiSelectable {
 
     public void process(APDU apdu) {
         byte[] reply = new byte[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+
+        //region TODO example use of JCMathlib
+        point1.randomize(); // Generate first point at random
+        point2.randomize();
+        // point2.setW(ECPOINT_TEST_VALUE, (short) 0, (short) ECPOINT_TEST_VALUE.length); // Set second point to predefined value
+        point1.add(point2); // Add two points together
+
+        // TODO BUG mvondracek: `ECPoint#multiplication` hangs in `applet.jcmathlib.Bignat.sqrt_FP`
+        //      on line 1738 in while loop. Is that simulator or computer specific?
+        //      point1.multiplication(SCALAR_TEST_VALUE, (short) 0, (short) SCALAR_TEST_VALUE.length);
+        //      byte[] test_scalar = new byte[]{(byte)2};
+        //      point1.multiplication(test_scalar, (short) 0, (short) test_scalar.length);
+
+        //endregion
+
         Util.arrayCopyNonAtomic(
             reply, (short) 0,
             apdu.getBuffer(), (short) 0,
@@ -134,5 +170,8 @@ public class EmeraldApplet extends Applet implements MultiSelectable {
      */
     private void clearSessionData() {
         // TODO overwrite session data in RAM with random data
+        //region TODO example use of JCMathlib
+        ecc.refreshAfterReset();
+        //endregion
     }
 }
