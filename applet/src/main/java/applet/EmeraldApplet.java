845f5d683e9bf084e870cb5485e3cc0a5db20863
Martin Vondráček <vondracek.mar@gmail.com>
2020-04-20 05:28:07 +0200

    added PIN installation and storage TODO javacard.framework.OwnerPIN is not suitable as we need to read PIN value for J-PAKE

--- applet/src/main/java/applet/EmeraldApplet.java
+++ applet/src/main/java/applet/EmeraldApplet.java
@@ -7,16 +7,80 @@ Team Emerald (in alphabetical order):
 
 package applet;
 
-import javacard.framework.APDU;
-import javacard.framework.Applet;
-import javacard.framework.MultiSelectable;
-import javacard.framework.Util;
+import javacard.framework.*;
 
 public class EmeraldApplet extends Applet implements MultiSelectable {
-    public EmeraldApplet(byte[] buffer, short offset, byte length) {
+    public static final byte PIN_LENGTH = 4;
+
+    /**
+     * Persistent PIN storage in EEPROM.
+     * TODO `javacard.framework.OwnerPIN` is not suitable as we need to read PIN value for J-PAKE
+     */
+    private final byte[] pin; // TODO store pin securely
+
+
+    /**
+     * Create instance of the applet.
+     *
+     * <p>Should be used from {@link #install} method. See {@link #install} for description of
+     * parameters. Calls {@link #register()} on successful initialization.
+     */
+    EmeraldApplet(byte[] bArray, short bOffset, byte bLength) {
+        byte instanceAIDLength = bArray[bOffset];
+        byte controlInfoLength = bArray[bOffset + 1 + instanceAIDLength];
+        byte appletDataLength = bArray[bOffset + 1 + instanceAIDLength + 1 + controlInfoLength];
+
+        short appletDataOffset = (short) (bOffset + 1 + instanceAIDLength + 1 + controlInfoLength + 1);
+        // first byte of applet data:
+        // bArray[bOffset + 1 + instanceAIDLength + 1 + controlInfoLength + 1]
+        // last byte of applet data:
+        // bArray[bOffset + 1 + instanceAIDLength + 1 + controlInfoLength + 1 + appletDataLength - 1]
+
+        // check if we have received PIN
+        if (appletDataLength != PIN_LENGTH) {
+            throw new IllegalArgumentException(
+                String.format("Unexpected length of applet data, expected=%s, actual=%s",
+                    PIN_LENGTH, appletDataLength));
+        }
+        // check if bytes of received PIN are only digits <0;9>
+        for (int i = 0; i < PIN_LENGTH; i++) {
+            byte pinDigit = bArray[appletDataOffset + i];
+            if(! (0 <= pinDigit && pinDigit <= 9)){
+                throw new IllegalArgumentException(
+                    String.format("Byte value of PIN digit not in range <0;9>, actual=%d", pinDigit));
+            }
+        }
+
+        // create persistent pin in EEPROM
+        pin = new byte[PIN_LENGTH];
+        Util.arrayCopy(bArray, appletDataOffset, pin, (short) 0, PIN_LENGTH);
+
+        // initialization successful
         register();
     }
 
+    /**
+     * Create an instance of the applet by the Java Card runtime environment.
+     *
+     * <p>Installation parameters are provided in bArray as follows:
+     * <ul>
+     *      <li>bArray[bOffset] = length(Li) of instance AID,
+     *      bArray[bOffset+1..bOffset+Li] = instance AID bytes,</li>
+     *      <li>bArray[bOffset+Li+1]= length(Lc) of control info,
+     *      bArray[bOffset+Li+2..bOffset+Li+Lc+1] = control info,</li>
+     *      <li>bArray[bOffset+Li+Lc+2] = length(La) of applet data,
+     *      bArray[bOffset+Li+Lc+3..bOffset+Li+Lc+La+2] = applet data</li>
+     * </ul>
+     * In the above format, any of the lengths: Li, Lc or La may be zero. The control information is
+     * implementation dependent. The bArray object is a global array. If the applet desires to
+     * preserve any of this data, it should copy the data into its own object. bArray is zeroed by
+     * the Java Card runtime environment after the return from the install() method.
+     *
+     * @param bArray  the array containing installation parameters
+     * @param bOffset the starting offset in bArray
+     * @param bLength the length in bytes of the parameter data in bArray The maximum value of
+     *                bLength is 127.
+     */
     public static void install(byte[] bArray, short bOffset, byte bLength) {
         new EmeraldApplet(bArray, bOffset, bLength);
     }
