e2ff162ee3ecfc86abe922ee98a487d992b41118
Oldrich Florian <oldrich.florian@gmail.com>
2020-04-21 23:01:42 +0200

    test verifyFalse

--- applet/src/test/java/applet/SchnorrZKPTest.java
+++ applet/src/test/java/applet/SchnorrZKPTest.java
@@ -7,21 +7,23 @@ import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class SchnorrZKPTest {
+    // init curve
+    ECNamedCurveParameterSpec curvespec =
+        ECNamedCurveTable.getParameterSpec("curve25519");
+    ECPoint G = curvespec.getG();
+    BigInteger n = curvespec.getN();
+    BigInteger coFactor = curvespec.getH();
+
+    //init alice
+    byte[] aliceName = {'a', 'l', 'i', 'c', 'e'}; // bob knows that it is alice
+
     @Test
     void testVerifyTrue() {
-        // init curve
-        ECNamedCurveParameterSpec curvespec =
-            ECNamedCurveTable.getParameterSpec("curve25519");
-        ECPoint G = curvespec.getG();
-        BigInteger n = curvespec.getN();
-        BigInteger coFactor = curvespec.getH();
         BigInteger x1 = ZKPUtils.generateRandomBigInteger(n);
-
-        //init alice
-        byte[] aliceName = {'a', 'l', 'i', 'c', 'e'}; // bob knows that it is alice
         SchnorrZKP alice = new SchnorrZKP(G, n, coFactor, x1, aliceName);
 
         //bob gets get alice's public keys and ZKP result
@@ -34,5 +36,22 @@ public class SchnorrZKPTest {
 
         //bob verifies
         assertTrue(ZKPUtils.verify(aliceA, G, aliceV, coFactor, aliceResult, aliceChallenge));
+    }
+
+    @Test
+    void testVerifyFalse() {
+        BigInteger x1 = ZKPUtils.generateRandomBigInteger(n);
+        SchnorrZKP alice = new SchnorrZKP(G, n, coFactor, x1, aliceName);
+
+        //bob gets get alice's public keys and ZKP result
+        ECPoint aliceV = G; // this will make it false
+        ECPoint aliceA = alice.getPublicA();
+        BigInteger aliceResult = alice.getResult();
+
+        // bob computes a challenge with alice's public keys, ZKP result and the public parameters
+        BigInteger aliceChallenge = ZKPUtils.computeChallenge(G, aliceV, aliceA, aliceName);
+
+        //bob verifies
+        assertFalse(ZKPUtils.verify(aliceA, G, aliceV, coFactor, aliceResult, aliceChallenge));
     }
 }
