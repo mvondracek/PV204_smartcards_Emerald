c1ae470e9a89fcd0460c42c24f806de073539015
Martin Vondráček <vondracek.mar@gmail.com>
2020-04-22 02:19:08 +0200

    gradle: multi-project build: separated emApplicationCli

---
+++ emApplication/src/main/java/emApplication/EmeraldApplicationCli.java
@@ -0,0 +1,16 @@
+/*
+Team Emerald (in alphabetical order):
+@OTristanF https://github.com/OTristanF
+@lsolodkova https://github.com/lsolodkova
+@mvondracek https://github.com/mvondracek
+ */
+
+package emApplication;
+
+
+public class EmeraldApplicationCli {
+
+    public static void main(String[] args) {
+        System.out.println("exit");
+    }
+}
