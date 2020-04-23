2769a05cdbb34216e157f51f7a1cc5d561b222ba
Martin Vondráček <vondracek.mar@gmail.com>
2020-04-23 04:21:25 +0200

    Code Climate: fix javadoc

--- applet/src/main/java/applet/EmeraldProtocolException.java
+++ applet/src/main/java/applet/EmeraldProtocolException.java
@@ -2,12 +2,12 @@ package applet;
 
 /**
  * Exception signaling that error occurred during Emerald protocol.
- * <p>
- * This can be caused by unexpected APDU, incorrect values during key agreement
+ *
+ * <p>This can be caused by unexpected APDU, incorrect values during key agreement
  * ({@link SecureChannelManagerBase} and subclasses), invalid integrity of encrypted messages,
  * incorrect values of messages, unexpected repeated APDU, ...
- * <p>
- * <b>This exception raises security alert.</b>
+ * 
+ * <p><b>This exception raises security alert.</b>
  */
 public class EmeraldProtocolException extends Exception {
 
